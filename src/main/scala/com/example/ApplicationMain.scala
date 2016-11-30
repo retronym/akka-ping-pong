dff314e11ef1163a50afa1f3fd07a38fd866d8f8
Johan Andrén <johan@markatta.com>
2016-11-30 14:24:32 +0100

    Single thread dispatcher

--- src/main/scala/com/example/ApplicationMain.scala
+++ src/main/scala/com/example/ApplicationMain.scala
@@ -1,9 +1,24 @@
 package com.example
 
 import akka.actor.ActorSystem
+import com.typesafe.config.ConfigFactory
 
 object ApplicationMain extends App {
-  val system = ActorSystem("MyActorSystem")
+  val config = ConfigFactory.parseString(
+    """
+      |akka {
+      |  actor {
+      |    default-dispatcher {
+      |      executor = "thread-pool-executor"
+      |      thread-pool-executor {
+      |        core-pool-size-min = 1
+      |        core-pool-size-max = 1
+      |      }
+      |    }
+      |  }
+      |}
+    """.stripMargin)
+  val system = ActorSystem("MyActorSystem", config)
   val pingActor = system.actorOf(PingActor.props, "pingActor")
   pingActor ! PingActor.Initialize
   // This example app will ping pong 3 times and thereafter terminate the ActorSystem - 
